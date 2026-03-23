a4c2ea888d7f01d262660426336bdcc08c8a85d6
CalixtePas <calixte.pasquet@gmail.com>
2026-03-23 14:07:54 -0400

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,8 @@
 import { buildServer } from './app.ts';
 
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
 const app = buildServer();
 
-const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
-
 app.listen(PORT).then(() => {
-  console.log(`Calixte server listening on port ${PORT}`);
+  console.log(`Castor server listening on port ${PORT}`);
 });
